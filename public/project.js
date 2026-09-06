14536e185e3c3cac317fcb017d14eae4a9b6d33a
daveboy3100-beep <daveboy3.100@gmail.com>
2026-09-06 23:10:36 +0100

    feat: read project details from url

--- public/project.js
+++ public/project.js
@@ -3,7 +3,16 @@ const projectTitle =
 
 const projectDescription =
   document.getElementById("projectDescription");
+const projectParams =
+  new URLSearchParams(
+    window.location.search
+  );
 
+const projectName =
+  projectParams.get("name");
+
+const projectDescriptionText =
+  projectParams.get("description");
 console.log("VoiceNest Project Workspace loaded");
 
 document.addEventListener(
